cc5b53123e59e3892a19a098d47e49fe9e0431f8
Pratikkumar201 <122555565+Pratikkumar201@users.noreply.github.com>
2025-06-11 14:29:25 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
     const formData = new FormData()
     formData.append('text', text)
 
-    const res = await fetch('https://qr-code-generator-ij17.onrender.com', {
+    const res = await fetch('https://qr-code-generator-ij17.onrender.com/generate_qr', {
       method: 'POST',
       body: formData,
     })
